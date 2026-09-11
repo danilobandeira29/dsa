31abca9910914a0cb260fd08f8ff39b0bb4f9e9a
Danilo Bandeira <61798805+danilobandeira29@users.noreply.github.com>
2026-09-10 23:44:33 -0300

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.go
@@ -0,0 +1,16 @@
+func productExceptSelf(nums []int) []int {
+	prefix := make([]int, len(nums))
+	prefix[0] = 1
+	for i := 1; i < len(nums); i++ {
+		prefix[i] = prefix[i - 1] * nums[i - 1]
+	}
+	posfix := make([]int, len(nums))
+	posfix[len(nums) - 1] = 1
+	for i := len(nums) - 2; i >= 0; i-- {
+		posfix[i] = posfix[i + 1] * nums[i + 1]
+	}
+	for i := range prefix {
+		prefix[i] *= posfix[i]
+	}
+	return prefix
+}
